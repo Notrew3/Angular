baf620f8d970f0dc676259bdfc38ea0ac244e289
Ewerton Azevedo <ewerton.azevedo@hotmail.com>
2018-10-17 14:44:14 -0300

    CursosService - Método getCursos()

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,18 +1,25 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
+
 export class CursosComponent implements OnInit {
 
 	nomePortal: string;
 
-	cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+	cursos: string[];
 
-  constructor() { 
+  constructor(private cursosService: CursosService) { 
   	this.nomePortal = 'http://loiane.training';
+
+  	//var servico = new CrusosService();
+  	this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
